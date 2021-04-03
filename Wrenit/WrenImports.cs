29f304cbb8c4da4d102146ea65d1b94ca3acee5b
Joram <joram@triangle-factory.be>
2021-04-03 17:59:00 +0200

    removed wrentypes for the eventual conflict with the c enum

--- Wrenit/WrenImports.cs
+++ Wrenit/WrenImports.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 namespace Wrenit
 {
-	public static class WrenImport
+	internal static class WrenImport
 	{
 
 #if DEBUG
@@ -14,7 +14,7 @@ namespace Wrenit
 #endif
 
 		#region Imports
-
+		
 		[DllImport(wrenDll)]
 		internal extern static void xWrenInitConfiguration([Out] WrenConfig config);
 
@@ -35,8 +35,91 @@ namespace Wrenit
 
 		#endregion
 
-		#region Types/Delegates
-
-		#endregion
 	}
+
+	#region Types/Delegates
+
+	internal delegate IntPtr WrenReallocateFn(IntPtr memory, UIntPtr size, IntPtr userData);
+
+	internal delegate void WrenWriteFn(IntPtr vm,
+	[MarshalAs(UnmanagedType.LPStr)] string text);
+
+	internal delegate void WrenErrorFn(IntPtr vm,
+		WrenErrorType type,
+		[MarshalAs(UnmanagedType.LPStr)] string module,
+		int line,
+		[MarshalAs(UnmanagedType.LPStr)] string message);
+
+	internal delegate IntPtr WrenResolveModuleFn(IntPtr vm,
+		[MarshalAs(UnmanagedType.LPStr)] string importer,
+		IntPtr name);
+
+	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+	internal delegate LoadModuleResult WrenLoadModuleFn(IntPtr vm,
+		[MarshalAs(UnmanagedType.LPStr)] string name);
+
+	internal delegate void WrenLoadModuleCompleteFn(IntPtr vm,
+		[MarshalAs(UnmanagedType.LPStr)] string name,
+		LoadModuleResult result);
+
+	[StructLayout(LayoutKind.Sequential)]
+	internal class LoadModuleResult
+	{
+		[MarshalAs(UnmanagedType.LPStr)]
+		public string source;
+
+		[MarshalAs(UnmanagedType.FunctionPtr)]
+		public WrenLoadModuleCompleteFn onComplete;
+
+		public IntPtr userData;
+	}
+
+	internal enum WrenErrorType
+	{
+		WREN_ERROR_COMPILE,
+		WREN_ERROR_RUNTIME,
+		WREN_ERROR_STACK_TRACE
+	}
+
+	internal enum WrenInterpretResult
+	{
+		SUCCESS,
+		WREN_ERROR_COMPILE,
+		WREN_ERROR_RUNTIME,
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	internal class WrenConfig
+	{
+		[MarshalAs(UnmanagedType.FunctionPtr)]
+		public WrenReallocateFn ReallocateFn;
+
+		[MarshalAs(UnmanagedType.FunctionPtr)]
+		public WrenResolveModuleFn ResolveModuleFn;
+
+		[MarshalAs(UnmanagedType.FunctionPtr)]
+		public WrenLoadModuleFn LoadModuleFn;
+
+		[MarshalAs(UnmanagedType.FunctionPtr)]
+		public WrenBindForeignMethodFn bindForeignMethodFn;
+
+		[MarshalAs(UnmanagedType.FunctionPtr)]
+		public WrenBindForeignClassFn bindForeignClassFn;
+
+		[MarshalAs(UnmanagedType.FunctionPtr)]
+		public WrenWriteFn WrenWriteFn;
+
+		[MarshalAs(UnmanagedType.FunctionPtr)]
+		public WrenErrorFn WrenErrorFn;
+
+		public UIntPtr initialHeapSize;
+
+		public UIntPtr minHeapSize;
+
+		public int heapGrowthPercent;
+
+		public IntPtr userData;
+	}
+	#endregion
+
 }
